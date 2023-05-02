96a8db015c699e79955829cc6088269d5bc214bf
Tesierroe <tesierroe@gmail.com>
2023-05-02 15:58:53 +0300

    Tasks for lesson 6

--- lesson_6/main.py
+++ lesson_6/main.py
@@ -1,10 +1,30 @@
-from lesson_6 import primer, n
-from lesson_6 import square, square_side
+from lesson_6 import primer, result
+from lesson_6 import square, sides
+from lesson_6 import is_prime, my_result
+from lesson_6 import change_list, ma_list
+from lesson_6 import to_dict, my_dict
+from lesson_6 import sum_range, number1, number2
 
-
+# task 1
 if __name__ == '__main__':
-    primer(n)
+    primer(result)
 
-
+# task 2
 if __name__ == '__main__':
-    square(square_side)
+    square(sides)
+
+# task 3
+if __name__ == '__main__':
+    is_prime(my_result)
+
+# task 4
+if __name__ == '__main__':
+    change_list(ma_list)
+
+# task 5
+if __name__ == '__main__':
+    to_dict(my_dict)
+
+# task 6
+if __name__ == '__main__':
+    sum_range(number1, number2)
